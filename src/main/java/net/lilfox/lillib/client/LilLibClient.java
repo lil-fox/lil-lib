c9d965a9c70f0750e9f483b3c47f0823a8a154a8
lil-fox <qute.lil.fox@gmail.com>
2025-03-12 18:22:49 +0100

    Add easter egg

--- src/main/java/net/lilfox/lillib/client/LilLibClient.java
+++ src/main/java/net/lilfox/lillib/client/LilLibClient.java
@@ -1,10 +1,12 @@
 package net.lilfox.lillib.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.lilfox.lillib.utils.ParticleUtils;
 
 public class LilLibClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        ParticleUtils.init();
     }
 }
