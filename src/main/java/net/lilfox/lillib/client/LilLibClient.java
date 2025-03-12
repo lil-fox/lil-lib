bec9ca06efcf7af529752bafbbfae7976241c429
lil-fox <qute.lil.fox@gmail.com>
2025-03-12 17:14:49 +0100

    init

---
+++ src/main/java/net/lilfox/lillib/client/LilLibClient.java
@@ -0,0 +1,10 @@
+package net.lilfox.lillib.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LilLibClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
