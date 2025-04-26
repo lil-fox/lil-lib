8740033d76bcac793e48cf8bb0ddb5d10db6281c
lil-fox <qute.lil.fox@gmail.com>
2025-04-26 18:47:28 +0200

    Update to 1.21.5

--- src/main/java/net/lilfox/lillib/mixin/LivingEntityMixin.java
+++ src/main/java/net/lilfox/lillib/mixin/LivingEntityMixin.java
@@ -1,8 +1,5 @@
 package net.lilfox.lillib.mixin;
 
-
-
-
 import net.lilfox.lillib.utils.ParticleUtils;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -21,8 +18,9 @@ public abstract class LivingEntityMixin {
             if (((PlayerEntity) (Object) this).getName().getString().equals("lil_fox")) {
 
                     if (!((PlayerEntity) (Object) this).isInvisible()) {
-                        ((LivingEntity) (Object) this).getWorld().addParticle(ParticleTypes.CHERRY_LEAVES, ((LivingEntity) (Object) this).getParticleX(5), ((LivingEntity) (Object) this).getRandomBodyY() + 1, ((LivingEntity) (Object) this).getParticleZ(5), 1.0, 1.0, 1.0);
-                        ((LivingEntity) (Object) this).getWorld().addParticle(ParticleTypes.CHERRY_LEAVES, ((LivingEntity) (Object) this).getParticleX(3), ((LivingEntity) (Object) this).getRandomBodyY(), ((LivingEntity) (Object) this).getParticleZ(3), -1.0, -1.0, -1.0);
+
+                        ((LivingEntity) (Object) this).getWorld().addParticleClient(ParticleTypes.CHERRY_LEAVES, ((LivingEntity) (Object) this).getParticleX(5), ((LivingEntity) (Object) this).getRandomBodyY() + 1, ((LivingEntity) (Object) this).getParticleZ(5), 1.0, 1.0, 1.0);
+                        ((LivingEntity) (Object) this).getWorld().addParticleClient(ParticleTypes.CHERRY_LEAVES, ((LivingEntity) (Object) this).getParticleX(3), ((LivingEntity) (Object) this).getRandomBodyY(), ((LivingEntity) (Object) this).getParticleZ(3), -1.0, -1.0, -1.0);
                     }else if (!((PlayerEntity) (Object) this).isSneaking()) {
                         ParticleUtils.addParticleSet("cherry",
                                 ((PlayerEntity) (Object) this), 16, 0.5, 0 , 0.5 , 0, 0 ,0);
